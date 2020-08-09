7fe13acbc953844916bda0b6ad1c3f1ceb6bf972
fw-amgaa <jasqwe6008@gmail.com>
2020-08-09 19:38:31 +0800

    erg

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -21,7 +21,7 @@
         <link href="vendors/revolution/css/layers.css" rel="stylesheet"> 
         <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
            
-        <!-- Extra plugin css -->
+        <!-- Extra plugin css -->  
         <link href="vendors/animate-css/animate.css" rel="stylesheet">   
         <link href="vendors/scroll/jquery.mCustomScrollbar.css" rel="stylesheet">
         <link href="vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
