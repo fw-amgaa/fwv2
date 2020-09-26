30bbf5102114f87a84b512df540e3da604fdc7f1
fw-amgaa <jasqwe6008@gmail.com>
2020-09-26 19:51:37 +0800

    oterihj

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -115,37 +115,75 @@
                             </div>
                             <div class="loop-list loop-list-style-1">
                                 <div class="row">
-                                @foreach($interviewBlogs as $blog)
-                                    <article class="col-md-6 mb-40 wow fadeInUp  animated">
-                                        <div class="post-card-1 border-radius-10 hover-up">
-                                            <div class="post-thumb thumb-overlay img-hover-slide position-relative" style="background-image: url({{ Voyager::image($blog->image) }})">
-                                                <a class="img-link" href="/blogs/{{ $blog->slug }}"></a>
-                                                <ul class="social-share">
-                                                    <li><a href="#"><i class="elegant-icon social_share"></i></a></li>
-                                                    <li><a class="fb" href="#" title="Share on Facebook" target="_blank"><i class="elegant-icon social_facebook"></i></a></li>
-                                                    <li><a class="tw" href="#" target="_blank" title="Tweet now"><i class="elegant-icon social_twitter"></i></a></li>
-                                                </ul>
+                                @foreach($blogs as $blog)
+                                <article class="col-md-6 mb-40 wow fadeInUp  animated">
+                                    <div class="post-card-1 border-radius-10 hover-up">
+                                        <div class="post-thumb thumb-overlay img-hover-slide position-relative" style="background-image: url({{Voyager::image($blog->image)}})">
+                                            <a class="img-link" href="/blogs/{{$blog->slug}}"></a>
+                                            <ul class="social-share">
+                                                <li><a href="#"><i class="elegant-icon social_share"></i></a></li>
+                                                <li><a class="fb" href="#" title="Share on Facebook" target="_blank"><i class="elegant-icon social_facebook"></i></a></li>
+                                                <li><a class="tw" href="#" target="_blank" title="Tweet now"><i class="elegant-icon social_twitter"></i></a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="post-content p-30">
+                                            <div class="entry-meta meta-0 font-small mb-10">
+                                                <a href="/academy"><span class="post-cat text-info">FW Academy</span></a>
+                                                <a href="/blogs/books"><span class="post-cat text-success">Book (For example)</span></a>
                                             </div>
-                                            <div class="post-content p-30">
-                                                <div class="entry-meta meta-0 font-small mb-10">
-                                                    <a href="/blogs"><span class="post-cat text-info">Blogs</span></a>
-                                                    <a href="/blogs/interviews"><span class="post-cat text-success">Interviews</span></a>
+                                            <div class="d-flex post-card-content">
+                                                <h5 class="post-title mb-20 font-weight-900">
+                                                    <a href="/blogs/{{$blog->slug}}">{{$blog->title}}</a>
+                                                </h5>
+                                                <div class="post-excerpt mb-25 font-small text-muted">
+                                                    <p>{{$blog->excerpt}}</p>
                                                 </div>
-                                                <div class="d-flex post-card-content">
-                                                    <h5 class="post-title mb-20 font-weight-900">
-                                                        <a href="/blogs/{{ $blog->slug }}">{{$blog->title}} </a>
-                                                    </h5>
-                                                    <div class="post-excerpt mb-25 font-small text-muted">
-                                                        <p>{{$blog->excerpt}} </p>
-                                                    </div>
-                                                    <div class="entry-meta meta-1 float-left font-x-small text-uppercase">
-                                                        <span class="post-on">{{$blog->created_at}}</span>
-                                                        <span class="time-reading has-dot">{{$blog->read}} mins read</span>
-                                                    </div>
+                                                <div class="entry-meta meta-1 float-left font-x-small text-uppercase">
+                                                    <span class="post-on">{{$blog->created_at}}</span>
+                                                    <span class="time-reading has-dot">{{$blog->read}} mins read</span>
                                                 </div>
                                             </div>
                                         </div>
-                                    </article>
+                                    </div>
+                                </article>
+                                @endforeach
+                                </div>
+                            </div>
+                        </div>
+                        <div class="post-module-2">
+                            <div class="widget-header-1 position-relative mb-30  wow fadeInUp animated">
+                                <h5 class="mt-5 mb-30">FW Publish</h5>
+                            </div>
+                            <div class="loop-list loop-list-style-1">
+                                <div class="row">
+                                @foreach($blogs as $blog)
+                                <article class="grid-item col pb-50 wow fadeIn animated">
+                                    <div class="post-card-1 border-radius-10 hover-up">
+                                        <div class="post-thumb thumb-overlay img-hover-slide position-relative" style="background-image: url({{ Voyager::image($blog->image) }})">
+                                            <a class="img-link" href="/blogs/{{$blog->slug}}"></a>
+                                            <span class="top-right-icon bg-success"><i class="elegant-icon icon_camera_alt"></i></span>
+                                            <ul class="social-share">
+                                                <li><a href="#"><i class="elegant-icon social_share"></i></a></li>
+                                                <li><a class="fb" href="#" title="Share on Facebook" target="_blank"><i class="elegant-icon social_facebook"></i></a></li>
+                                                <li><a class="tw" href="#" target="_blank" title="Tweet now"><i class="elegant-icon social_twitter"></i></a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="post-content p-30">
+                                            <div class="entry-meta meta-0 font-small mb-10">
+                                                <a href="/publish"><span class="post-cat text-info">FW Publish</span></a>
+                                            </div>
+                                            <div class="d-flex post-card-content">
+                                                <h5 class="post-title mb-20 font-weight-900">
+                                                    <a href="/blogs/{{$blog->title}}">{{$blog->title}}</a>
+                                                </h5>
+                                                <div class="entry-meta meta-1 float-left font-x-small text-uppercase">
+                                                    <span class="post-on">{{$blog->created_at}}</span>
+                                                    <span class="time-reading has-dot">{{$blog->read}} mins read</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
                                 @endforeach
                                 </div>
                             </div>
